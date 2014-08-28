e322dfc713ced35dbd8ce51dd7127583a69a61b3
Colin <cdbusby@gmail.com>
2014-08-28 10:16:09 -0300

    Added organization by month when querying by year, added results limit as per Strava API limits, and a few other small fixes.

--- TrackerApp.php
+++ TrackerApp.php
@@ -1,120 +1,125 @@
 <?php
 
-// Include Strava PHP API
 require 'external/StravaApi.php';
 
 class TrackerApp {
 
-    /**
-     * Sets up the class and initialized StravaApi
-     * @param array $creds
-     * @param array $query
-     */
+  const MAXRESULTS = 200;
 
-    public function __construct( $creds, $query ) {
+  public function __construct($creds, $query) {
 
-        // Set variables
-        $this->clientId = $creds['clientId'];
-        $this->clientSecret = $creds['clientSecret'];
-        $this->accessToken = $creds['accessToken'];
+    $this->clientId = $creds['clientId'];
+    $this->clientSecret = $creds['clientSecret'];
+    $this->accessToken = $creds['accessToken'];
 
-        $this->query = $this->parseQuery( $query );
+    $this->query = $query;
+    $this->parsedQuery = $this->parseQuery($query);
 
-        $this->api = new StravaApi( $this->clientSecret, $this->clientId );
+    $this->api = new StravaApi($this->clientSecret, $this->clientId);
+
+  }
+
+  private function parseQuery($query) {
+
+    if (!array_key_exists("limit", $query)) {
+      $query["limit"] = self::MAXRESULTS;
+    }
+
+    $params = array();
+
+    foreach ($query as $key => $val) {
+
+      switch ($key) {
+        case "day":
+          $explode = explode("-", $val);
+          $day = $explode[0];
+          $month = $explode[1];
+          $year = $explode[2];
+
+          $first_minute = mktime(0, 0, 0, $month, $day, $year);
+          $last_minute = mktime(23, 59, 0, $month, $day, $year);
+
+          $params['after'] = $first_minute;
+          $params['before'] = $last_minute;
+          break;
+
+        case "month":
+          $explode = explode("-", $val);
+          $month = $explode[0];
+          $year = $explode[1];
+
+          $first_minute = mktime(0, 0, 0, $month, 1, $year);
+          $last_minute = mktime(23, 59, 0, $month, date("t"), $year);
+
+          $params['after'] = $first_minute;
+          $params['before'] = $last_minute;
+          break;
+
+        case "year":
+          $year = $val;
+
+          $first_minute = mktime(0, 0, 0, 1, 1, $year);
+          $last_minute = mktime(23, 59, 0, 12, date("t"), $year);
+
+          $params['after'] = $first_minute;
+          $params['before'] = $last_minute;
+          break;
+
+        case "limit":
+          if ($val > self::MAXRESULTS) {
+            $val = self::MAXRESULTS;;
+          }
+          $params['per_page'] = $val;
+          break;
+      }
 
     }
 
-    /**
-     * Gets a list of activities (JSON) based on query string and filters map results
-     * @param bool $filterMaps
-     * @return string
-     */
+    return $params;
 
-    public function listActivities( $filterMaps = true ) {
+  }
 
-        $activities = $this->api->get( "athlete/activities", $this->accessToken, $this->query );
+  public function listActivities($filterMaps = TRUE) {
 
-        if ( $filterMaps ) {
-            $activities = $this->filterMaps( $activities );
-        }
+    $activities = $this->api->get("athlete/activities", $this->accessToken, $this->parsedQuery);
 
-        return json_encode($activities);
+    if ($filterMaps) {
+      $activities = $this->filterMaps($activities);
+    }
+
+    if (array_key_exists("year", $this->query)) {
+      $activities = $this->groupByMonth($activities);
+    }
+
+    return json_encode($activities);
+
+  }
+
+  private function filterMaps($obj) {
+
+    foreach ($obj as &$item) {
+      unset($item->map);
+    }
+
+    return $obj;
+
+  }
+
+  private function groupByMonth($obj) {
+
+    $array = array();
+
+    foreach ($obj as &$item) {
+
+      $date = explode("-", $item->start_date);
+      $month = $date[1];
+
+      $array[$month][] = $item;
 
     }
 
-    /**
-     * Filters out mapping from api object
-     * @param object $obj
-     * @return object
-     */
+    return $array;
 
-    private function filterMaps( $obj ) {
-
-        foreach ( $obj as &$item ) {
-            unset( $item->map );
-        }
-
-        return $obj;
-
-    }
-
-    /**
-     * Parses the query string into a usable array
-     * @param array $query
-     * @return array
-     */
-
-    private function parseQuery( $query ) {
-
-        $params = array();
-
-        foreach( $query as $key => $val ) {
-
-            switch( $key ) {
-                case "day":
-                    $explode = explode( "-", $val );
-                    $day = $explode[0];
-                    $month = $explode[1];
-                    $year = $explode[2];
-
-                    $first_minute = mktime( 0, 0, 0, $month, $day, $year );
-                    $last_minute = mktime( 23, 59, 0, $month, $day, $year );
-
-                    $params['after'] = $first_minute;
-                    $params['before'] = $last_minute;
-                    break;
-
-                case "month":
-                    $explode = explode( "-", $val );
-                    $month = $explode[0];
-                    $year = $explode[1];
-
-                    $first_minute = mktime( 0, 0, 0, $month, 1, $year );
-                    $last_minute = mktime( 23, 59, 0, $month, date("t"), $year );
-
-                    $params['after'] = $first_minute;
-                    $params['before'] = $last_minute;
-                    break;
-
-                case "year":
-                    $year = $val;
-
-                    $first_minute = mktime( 0, 0, 0, 1, 1, $year );
-                    $last_minute = mktime( 23, 59, 0, 12, date("t"), $year );
-
-                    $params['after'] = $first_minute;
-                    $params['before'] = $last_minute;
-                    break;
-
-                case "limit":
-                    $params['per_page'] = $val;
-                    break;
-            }
-
-        }
-
-        return $params;
-
-    }
+  }
 
 }
